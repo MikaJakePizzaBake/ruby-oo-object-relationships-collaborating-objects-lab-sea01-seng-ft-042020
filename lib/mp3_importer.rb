6556cdba91d90bc1c32a1550c08ae0053b2b6d44
Trevor Low <tlow94@uw.edu>
2020-04-27 02:34:44 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -1,9 +1,11 @@
 class MP3Importer
-  attr_accessor :file_name
-  def initialize(file_name)
-    @file_name = file_name
+  attr_accessor :path
+  def initialize(path)
+    @path = path
   end
   def files
 
+  end
+  def import
   end
 end
